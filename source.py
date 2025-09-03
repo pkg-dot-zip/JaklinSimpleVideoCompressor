a29e9d2d06d37f483e38a8fd61ef76e34d4c199f
pkg-dot-zip <gitkraken.barge178@passmail.net>
2025-09-03 17:20:25 +0200

    Started working on the input class

---
+++ source.py
@@ -0,0 +1,38 @@
+import pathlib
+from enum import Enum
+
+
+class SourceType(Enum):
+    VIDEO = "video"
+    IMAGE = "image"
+    AUDIO = "audio"
+
+
+video_types = [".mp4", ".webm", ".avi"]
+image_types = [".jpg", ".jpeg", ".png", ".webp"]
+audio_types = [".mp3", ".wav", ".aiff"]
+
+
+def calc_sourc_type(extension: str):
+    """Returns enum with type of source file. Returns none if invalid."""
+
+    # The 'endswith()' method takes in tuples, not lists, according to SO.
+    video_tuple = tuple(video_types)
+    image_tuple = tuple(image_types)
+    audio_tuple = tuple(audio_types)
+
+    if extension.lower().endswith(video_tuple):
+        return SourceType.VIDEO
+    elif extension.lower().endswith(image_tuple):
+        return SourceType.IMAGE
+    elif extension.lower().endswith(audio_tuple):
+        return SourceType.AUDIO
+    return None
+
+
+class SourceFile:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.extension = pathlib.Path(filepath).suffix.lower()
+        self.name = pathlib.Path(filepath).stem
+        self.type = calc_sourc_type(self.extension)
